d719803e47fd9af8e02c7ee841ab0d97073f03de
Austin Chau <austin.chau1@gmail.com>
2021-09-01 16:53:12 -0400

    Added test for ship factory

--- src/tests/Ship.test.js
+++ src/tests/Ship.test.js
@@ -1,5 +1,9 @@
-import Ship from "../Ship";
+import Ship from "../factories/Ship";
 
-test('Ship takes a position', () => {
-  expect()
+const position = ['A1', 'A2', 'A3', 'A4'];
+const newShip = Ship(position);
+test('Ship takes a hit', () => {
+  newShip.hit('A2');
+  const { shipArray } = newShip;
+  expect(shipArray).toStrictEqual(['A1','hit','A3','A4']);
 })
