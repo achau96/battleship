215b4ea541aa13544e72452d0f64ba2d87d6c8c5
Austin Chau <austin.chau1@gmail.com>
2021-09-09 18:20:30 -0400

    Refactored click event into game loop, seperate from DOM Module

--- src/Game.js
+++ src/Game.js
@@ -10,6 +10,9 @@ const ships = [
   [false,2,'G6','smallShip']
 ];
 
+const hitCoordinate = (event) => {
+  console.log(event.target.dataset.value);
+}
 
 const Game = () => {
 const playerBoard = GameBoard(8);
@@ -20,7 +23,18 @@ playerBoard.placeShip(...ships[1]);
 playerBoard.placeShip(...ships[2]);
 playerBoard.placeShip(...ships[3]);
 playerBoard.placeShip(...ships[4]);
-console.log(playerBoard.board);
+
+enemyBoard.placeShip(...ships[0]);
+enemyBoard.placeShip(...ships[1]);
+enemyBoard.placeShip(...ships[2]);
+enemyBoard.placeShip(...ships[3]);
+enemyBoard.placeShip(...ships[4]);
+
+const enemyDOM = document.getElementById('enemy');
+const enemyTiles = enemyDOM.querySelectorAll('.tile');
+enemyTiles.forEach((tile) => {
+  tile.addEventListener('click',hitCoordinate);
+})
 //start game
 //game figures out whose turn it is, and waits for event listener if player
 //or makes random move if opponent
--- src/index.js
+++ src/index.js
@@ -8,15 +8,13 @@ const hitCoordinate = (event) => {
 const createGameBoard = (size,player) => {
   const gameBoard = document.createElement('div');
   gameBoard.classList.add('gameBoard');
+  gameBoard.id = player;
   gameBoard.setAttribute('style', `grid-template-columns: repeat(${size},1fr);grid-template-rows: repeat(${size},1fr)`)
   for(let i=0;i<size;i++){
     for(let j=0;j<size;j++){
       const block = document.createElement('div');
       block.dataset.value = `${String.fromCharCode(i+65)}${j+1}`;
       block.classList.add('tile');
-      if(player === 'enemy'){
-      block.addEventListener('click', hitCoordinate);
-      }
       gameBoard.appendChild(block);
     }
   }
@@ -27,7 +25,7 @@ const App = () => {
   const player = document.querySelector('.player');
   const opponent = document.querySelector('.opponent');
 
-  player.appendChild(createGameBoard(8,'player'),);
+  player.appendChild(createGameBoard(8,'user'),);
   opponent.appendChild(createGameBoard(8,'enemy'));
 
   Game();
