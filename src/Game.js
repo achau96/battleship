b29680ab4193befe272d69568d08bef7e374c2a9
Austin Chau <austin.chau1@gmail.com>
2021-09-10 18:08:41 -0400

    Added more DOM manipulation for attacking enemy board

--- src/Game.js
+++ src/Game.js
@@ -5,26 +5,34 @@ import GameBoard from './factories/GameBoard';
 
 const hitCoordinate = (event,enemyBoard) => {
   const status = document.querySelector('.status');
-  status.textContent = 'Missed...';
-  status.classList.remove('show');
-  setTimeout(()=>{
-    status.classList.add('show');
-  },10)
+
   //should send coordinate into player, which goes to board and returns a hit or miss
   //and effects the DOM accordingly
-  console.log(enemyBoard)
   for(let i=0;i<enemyBoard.length;i++){
     enemyBoard[i].forEach((ship) => {
       if (ship.tile == event.target.dataset.value && ship.shipID!==null){
         console.log('ship hit!');
         event.target.textContent = 'X';
         event.target.classList.add('hit');
+        status.textContent = 'Hit!'
+      }
+      else if(ship.tile == event.target.dataset.value && ship.shipID===null){
+        console.log('ship missed');
+        event.target.textContent = 'O';
+        event.target.classList.add('miss');
+        status.textContent = 'Missed...';
       }
     })
   }
+  status.classList.remove('show');
+  setTimeout(()=>{
+    status.classList.add('show');
+  },10)
 }
 
 const Game = (playerBoard,enemyBoard) => {
+const player = Player();
+const AI = Player();
 let turn = 'player';
 const enemyDOM = document.getElementById('enemy');
 const enemyTiles = enemyDOM.querySelectorAll('.tile');
