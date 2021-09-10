f1d560814f98a3191b326cd017be4e13bd26063e
Austin Chau <austin.chau1@gmail.com>
2021-09-10 17:50:38 -0400

    created ship hit effect

--- src/Game.js
+++ src/Game.js
@@ -3,16 +3,25 @@ import Player from './factories/Player';
 import GameBoard from './factories/GameBoard';
 
 
-const hitCoordinate = (event) => {
+const hitCoordinate = (event,enemyBoard) => {
   const status = document.querySelector('.status');
-  status.textContent = 'Player Missed';
-  status.style.display = 'block';
+  status.textContent = 'Missed...';
+  status.classList.remove('show');
   setTimeout(()=>{
-    status.style.display='none';
-  },2000)
+    status.classList.add('show');
+  },10)
   //should send coordinate into player, which goes to board and returns a hit or miss
   //and effects the DOM accordingly
-  console.log(event.target.dataset.value);
+  console.log(enemyBoard)
+  for(let i=0;i<enemyBoard.length;i++){
+    enemyBoard[i].forEach((ship) => {
+      if (ship.tile == event.target.dataset.value && ship.shipID!==null){
+        console.log('ship hit!');
+        event.target.textContent = 'X';
+        event.target.classList.add('hit');
+      }
+    })
+  }
 }
 
 const Game = (playerBoard,enemyBoard) => {
@@ -24,7 +33,7 @@ const enemyTiles = enemyDOM.querySelectorAll('.tile');
 //or makes random move if opponent
 if(turn === 'player'){
   enemyTiles.forEach((tile) => {
-    tile.addEventListener('click',hitCoordinate, {once:true});
+    tile.addEventListener('click',(event) => hitCoordinate(event,enemyBoard.board), {once:true});
   })
 }
 
