c1a52c3e49c95248b6d627a6a991cbeb11610c4b
Austin Chau <austin.chau1@gmail.com>
2021-09-10 15:01:59 -0400

    Moved initializing game out of game module

--- src/Game.js
+++ src/Game.js
@@ -1,14 +1,7 @@
 import Ship from './factories/Ship';
 import Player from './factories/Player';
-import GameBoard from './factories/GameBoard'
+import GameBoard from './factories/GameBoard';
 
-const ships = [
-  [false, 5, 'A2','commander'],
-  [true, 4, 'B1','subCommander'],
-  [false,3,'B4','lieutenant'],
-  [true,3,'E3','general'],
-  [false,2,'G6','smallShip']
-];
 
 const hitCoordinate = (event) => {
   //should send coordinate into player, which goes to board and returns a hit or miss
@@ -17,29 +10,19 @@ const hitCoordinate = (event) => {
 }
 
 const Game = () => {
-const playerBoard = GameBoard(8);
-const enemyBoard = GameBoard(8);
-//place ships
-playerBoard.placeShip(...ships[0]);
-playerBoard.placeShip(...ships[1]);
-playerBoard.placeShip(...ships[2]);
-playerBoard.placeShip(...ships[3]);
-playerBoard.placeShip(...ships[4]);
-
-enemyBoard.placeShip(...ships[0]);
-enemyBoard.placeShip(...ships[1]);
-enemyBoard.placeShip(...ships[2]);
-enemyBoard.placeShip(...ships[3]);
-enemyBoard.placeShip(...ships[4]);
 
 const enemyDOM = document.getElementById('enemy');
 const enemyTiles = enemyDOM.querySelectorAll('.tile');
-enemyTiles.forEach((tile) => {
-  tile.addEventListener('click',hitCoordinate, {once:true});
-})
+const playerDOM = document.getElementById('user');
+const playerTiles = playerDOM.querySelectorAll('.tile');
+
 //start game
 //game figures out whose turn it is, and waits for event listener if player
 //or makes random move if opponent
+
+enemyTiles.forEach((tile) => {
+  tile.addEventListener('click',hitCoordinate, {once:true});
+})
 //Check status if ship is hit, check if all ships are sunk and display winner
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import './index.css'
+import GameBoard from './factories/GameBoard';
 import Game from './Game'
+
+const ships = [
+  [false, 5, 'A2','commander'],
+  [true, 4, 'B1','subCommander'],
+  [false,3,'B4','lieutenant'],
+  [true,3,'E3','general'],
+  [false,2,'G6','smallShip']
+];
 
 const createGameBoard = (size,player) => {
   const gameBoard = document.createElement('div');
@@ -17,6 +26,25 @@ const createGameBoard = (size,player) => {
   return gameBoard;
 }
 
+const initialize = () => {
+const playerBoard = GameBoard(8);
+const enemyBoard = GameBoard(8);
+//place ships
+playerBoard.placeShip(...ships[0]);
+playerBoard.placeShip(...ships[1]);
+playerBoard.placeShip(...ships[2]);
+playerBoard.placeShip(...ships[3]);
+playerBoard.placeShip(...ships[4]);
+
+enemyBoard.placeShip(...ships[0]);
+enemyBoard.placeShip(...ships[1]);
+enemyBoard.placeShip(...ships[2]);
+enemyBoard.placeShip(...ships[3]);
+enemyBoard.placeShip(...ships[4]);
+
+return {playerBoard,enemyBoard}
+}
+
 const App = () => {
   const player = document.querySelector('.player');
   const opponent = document.querySelector('.opponent');
@@ -24,6 +52,8 @@ const App = () => {
   player.appendChild(createGameBoard(8,'user'),);
   opponent.appendChild(createGameBoard(8,'enemy'));
 
+  const {playerBoard,enemyBoard} = initialize();
+  console.log(playerBoard.board);
   Game();
 
 }
